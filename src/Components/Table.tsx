5767c060f3f9120b399b1f08060ed98c197bf5ff
ilkkamaksy <ijmakine@gmail.com>
2021-10-12 17:41:45 +0300

    add icons

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Participant } from '../types'
 import EditForm from './EditForm'
 import { useSortableData } from '../hooks/SortableData'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown, faArrowUp, faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 interface Props {
 	participants: Participant[]
@@ -35,9 +37,30 @@ const Table = ({
 			<div className="table data-table">
 				<div className="table-header-group">
 					<div className="table-row">
-						<div className={`table-cell table-cell--header-cell ${sortConfig && sortConfig.key === 'name' ? 'sorted' : ''}`} onClick={() => reSort('name')}>Name</div>
-						<div className={`table-cell table-cell--header-cell ${sortConfig && sortConfig.key === 'email' ? 'sorted' : ''}`} onClick={() => reSort('email')}>E-mail address</div>
-						<div className={`table-cell table-cell--header-cell ${sortConfig && sortConfig.key === 'phone' ? 'sorted' : ''}`} onClick={() => reSort('phone')}>Phone number</div>
+						<div 
+							className={`table-cell table-cell--header-cell ${sortConfig && sortConfig.key === 'name' ? 'sorted' : ''}`} 
+							onClick={() => reSort('name')}>
+								Name
+							{sortConfig && sortConfig.key === 'name' && sortConfig.direction === 'ascending' 
+								? <FontAwesomeIcon icon={faArrowUp} /> 
+								: sortConfig && sortConfig.key === 'name' ? <FontAwesomeIcon icon={faArrowDown} /> : null}
+						</div>
+						<div 
+							className={`table-cell table-cell--header-cell ${sortConfig && sortConfig.key === 'email' ? 'sorted' : ''}`} 
+							onClick={() => reSort('email')}>
+								E-mail address
+							{sortConfig && sortConfig.key === 'email' && sortConfig.direction === 'ascending' 
+								? <FontAwesomeIcon icon={faArrowUp} /> 
+								: sortConfig && sortConfig.key === 'email' ? <FontAwesomeIcon icon={faArrowDown} /> : null}
+						</div>
+						<div 
+							className={`table-cell table-cell--header-cell ${sortConfig && sortConfig.key === 'phone' ? 'sorted' : ''}`} 
+							onClick={() => reSort('phone')}>
+								Phone number
+							{sortConfig && sortConfig.key === 'phone' && sortConfig.direction === 'ascending' 
+								? <FontAwesomeIcon icon={faArrowUp} /> 
+								: sortConfig && sortConfig.key === 'phone' ? <FontAwesomeIcon icon={faArrowDown} /> : null}
+						</div>
 						<div className="table-cell table-cell--header-cell table-cell-end"></div>
 					</div>
 				</div>
@@ -59,8 +82,12 @@ const Table = ({
 									<div className="table-cell table-cell--data-cell" onClick={() => handleSelect(participant)}>{participant.email}</div>
 									<div className="table-cell table-cell--data-cell" onClick={() => handleSelect(participant)}>{participant.phone}</div>
 									<div className="table-cell table-cell--data-cell table-cell-end">
-										<button onClick={() => handleSelect(participant)}>Edit</button>
-										<button onClick={() => handleRemove(participant)}>Remove</button>
+										<button className="icon-btn" onClick={() => handleSelect(participant)}>
+											<FontAwesomeIcon icon={faPen} />
+										</button>
+										<button className="icon-btn" onClick={() => handleRemove(participant)}>
+											<FontAwesomeIcon icon={faTrash} />
+										</button>
 									</div>
 								</div>
 							)
